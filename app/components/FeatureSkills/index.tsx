40143f7425837391f7494db3d7bc78970ec311b3
Dan Cabaniss <dccabs@gmail.com>
2023-02-23 16:54:31 -0600

    Update resume

--- app/components/FeatureSkills/index.tsx
+++ app/components/FeatureSkills/index.tsx
@@ -1,70 +1,77 @@
-import {CodeBracketIcon, CodeBracketSquareIcon } from '@heroicons/react/24/outline'
+import {
+  CodeBracketIcon,
+  CodeBracketSquareIcon,
+} from "@heroicons/react/24/outline";
 import * as React from "react";
 
 const features = [
-    {
-        name: 'Tailwind CSS',
-        description:
-            'Expert level knowledge of Tailwind CSS. I believe it brings a much needed level of standardization to UI Layouts and styling',
-        icon: CodeBracketSquareIcon,
-    },
-    {
-        name: 'NextJS',
-        description:
-            'Expert level knowledge of NextJS. Server side rendering, static site generation, integrating APIs, serverless functions, and edge functions',
-        icon: CodeBracketIcon,
-    },
-    {
-        name: 'React',
-        description:
-            'Several years of experience building enterprise applications with React including hooks, context, and other lifecycle methods',
-        icon: CodeBracketIcon,
-    },
-    {
-        name: 'State Management',
-        description:
-            'Tanstack Query, Redux, Zustand, and other state management libraries',
-        icon: CodeBracketSquareIcon,
-    },
-]
+  {
+    name: "React",
+    description:
+      "Several years of experience building enterprise applications with React including hooks, context, and other lifecycle methods",
+    icon: CodeBracketIcon,
+  },
+  {
+    name: "NextJS",
+    description:
+      "Expert level knowledge of NextJS. Server side rendering, static site generation, integrating APIs, serverless functions, and edge functions",
+    icon: CodeBracketIcon,
+  },
+  {
+    name: "Tailwind CSS",
+    description:
+      "Expert level knowledge of Tailwind CSS. I believe it brings a much needed level of standardization to UI Layouts and styling",
+    icon: CodeBracketSquareIcon,
+  },
+  {
+    name: "State Management",
+    description:
+      "Tanstack Query, Redux, Zustand, and other state management libraries",
+    icon: CodeBracketSquareIcon,
+  },
+];
 
 interface FeatureProps {
-    title: string
+  title: string;
 }
 
 interface Feature {
-    name: string
-    description: string
-    icon: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>>
+  name: string;
+  description: string;
+  icon: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>>;
 }
-
 
 const Feature = (props: FeatureProps) => {
-    const { title } = props
-    return (
-        <div className="py-0 sm:py-0">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-4">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        {title}
-                    </h2>
-                    <dl className="col-span-3 grid grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2">
-                        {features.map((feature: Feature) => (
-                            <div key={feature.name}>
-                                <dt className="text-base font-semibold leading-7 text-gray-900 flex items-center mb-6">
-                                    <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600 mr-4">
-                                        <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
-                                    </div>
-                                    {feature.name}
-                                </dt>
-                                <dd className="mt-1 text-base leading-7 text-gray-600">{feature.description}</dd>
-                            </div>
-                        ))}
-                    </dl>
-                </div>
-            </div>
+  const { title } = props;
+  return (
+    <div className="py-0 sm:py-0">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            {title}
+          </h2>
+          <dl className="col-span-3 grid grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2">
+            {features.map((feature: Feature) => (
+              <div key={feature.name}>
+                <dt className="text-base font-semibold leading-7 text-gray-900 flex items-center mb-6">
+                  <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600 mr-4">
+                    <feature.icon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-1 text-base leading-7 text-gray-600">
+                  {feature.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Feature
+export default Feature;
